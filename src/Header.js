eca884de5b9e2244b0e10a89a856d1af6194d5bb
Navfal <muhammednavfal1@gmail.com>
2020-11-22 11:07:32 +0530

    front end app set

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css";
+import IconButton from "@material-ui/core/IconButton";
+import PersonButton from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+
+function Header() {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonButton fontSize="large" className="header__icon" />
+      </IconButton>
+      <img
+        className="header__logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+      />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
+    </div>
+  );
+}
+
+export default Header;
